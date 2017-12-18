0836d9a664929073b79d62c735f06de3be70a283
gpbacang <gpbacang@Geraldines-MacBook-Pro.local>
2017-12-18 10:10:05 +0400

    Add form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxStripeModule } from 'ngx-stripe';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxStripeModule.forRoot('pk_test_hDY91WuFrr8IsfCh91pxQq4w'),
   ],
   providers: [],
   bootstrap: [AppComponent]
